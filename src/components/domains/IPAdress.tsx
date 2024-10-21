6e3f3e1b77c345681991dd0b65ae2b1a73b6ef40
KadoProG <splatrain@gmail.com>
2024-10-21 09:13:41 +0900

    update コンポーネント導入

---
+++ src/components/domains/IPAdress.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { getLocalIpAddress } from '@/utils';
+import styles from '@/App.module.scss';
+
+export const IPAdress: React.FC = () => {
+  const [ipAdress, setIpAddress] = React.useState<string>('');
+  const [time, setTime] = React.useState<string>('');
+
+  React.useEffect(() => {
+    getLocalIpAddress.then((ip) => {
+      setIpAddress(ip);
+    });
+
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 10);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div className={styles.div}>
+      <div>
+        <p style={{ fontSize: '3vw' }}>あなたのローカルIPアドレスは、</p>
+        <p style={{ fontSize: '10vw' }}>{ipAdress}</p>
+
+        <p style={{ fontSize: '5vw' }}>{time}</p>
+        <button onClick={() => document.body.requestFullscreen()}>全画面表示</button>
+      </div>
+    </div>
+  );
+};
