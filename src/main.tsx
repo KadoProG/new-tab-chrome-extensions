f600105ecf7c5887a0cadcf7836064564636d13f
KadoProG <sho.kadoguchi@sun-asterisk.com>
2025-06-17 13:07:28 +0900

    feat: tailwindの導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { App } from '@/App.tsx';
 import '@/index.scss';
+import '@/index.css';
 import { NetworkStatusProvider } from '@/components/contexts/networkStatus/NetworkStatusProvider';
 import { LinkProvider } from '@/components/contexts/link/LinkProvider';
 import { LinkEditDialogProvider } from '@/components/contexts/llinkEditDialog/LinkEditDialogProvider';
